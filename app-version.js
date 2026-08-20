3add1f6b51670335ccaf85dd0b6d2306b8e8febc
simuladorcritica <heveva.27@hotmail.com>
2026-08-20 13:09:45 -0600

    Actualizar version visual del sitio

--- app-version.js
+++ app-version.js
@@ -1,2 +1,2 @@
-export const RT_WEB_VERSION = 'RT-web-2026.08.19.1';
-export const RT_WEB_RELEASED_AT = '2026-08-19T16:20:00-06:00';
+export const RT_WEB_VERSION = 'RT-web-2026.08.20.2';
+export const RT_WEB_RELEASED_AT = '2026-08-20T12:45:00-06:00';
