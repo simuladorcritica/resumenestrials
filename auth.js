7e9cd64f4c741626b8e92d142d969b6bcb796529
simuladorcritica <heveva.27@hotmail.com>
2026-08-20 13:07:42 -0600

    Aplicar sistema visual premium a paginas de cuenta

--- auth.js
+++ auth.js
@@ -1,3 +1,4 @@
+import "./member-design-v3.js";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import { SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY } from "./supabase-config.js";
 
