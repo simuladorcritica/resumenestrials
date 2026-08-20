54d80bb7a67f16e4eb9dac81307358717aebcb06
simuladorcritica <heveva.27@hotmail.com>
2026-08-20 14:07:20 -0600

    Versionar rediseño editorial integral 2026

--- app-version.js
+++ app-version.js
@@ -1,2 +1,2 @@
-export const RT_WEB_VERSION = 'RT-web-2026.08.20.2';
-export const RT_WEB_RELEASED_AT = '2026-08-20T12:45:00-06:00';
+export const RT_WEB_VERSION = 'RT-web-2026.08.20.3';
+export const RT_WEB_RELEASED_AT = '2026-08-20T14:05:00-06:00';
